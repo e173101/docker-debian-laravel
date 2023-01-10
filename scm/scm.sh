d9755e9a2b52eefe53468edb07efb3b4eb6f17db
Ryan Lai <Ryan.Lai@armchina.com>
2023-01-10 19:23:19 +0800

    add arg, fix update rule

--- scm/scm.sh
+++ scm/scm.sh
@@ -1,24 +1,28 @@
 #!/bin/bash -e
 
+GIT_URL=$1
+GIT_BRANCH=$2
+
 git init
-git pull --no-rebase https://gitee.com/yongtian/todo-pi-server.git master
+git pull --no-rebase $GIT_URL $GIT_BRANCH
 
 composer install
 cp .env.example .env
 php artisan key:generate
 
+echo "========== INIT DONE =========="
+
 while [ true ]
 do
     last_revision=$(git log -n 1 --format="%H")
-    remote_revision=$(git ls-remote -h https://gitee.com/yongtian/todo-pi-server.git master)
+    remote_revision=$(git ls-remote -h $GIT_URL $GIT_BRANCH)
 
-    if [[ $last_revision != $remote_revision* ]]; then
+    if [[ ${remote_revision} != *${last_revision}* ]]; then
         echo "========== UPDATE START =========="
         date
-        echo "========== UPDATE START =========="
 
         echo "last revision: $last_revision"
-        echo "remote revision: ${remote_revision}"
+        echo "     revision: ${remote_revision}"
         
         # update code
         git pull --no-rebase https://gitee.com/yongtian/todo-pi-server.git master
@@ -28,7 +32,6 @@ do
         php artisan view:clear
         # php artisan migrate
 
-        echo "========== UPDATE END =========="
         date
         echo "========== UPDATE END =========="
     fi
